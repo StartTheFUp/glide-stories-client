c2c129a5ff0b014dc0db36c5995e39aedc7fed8b
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-05-29 17:55:15 +0200

    first display of the sip

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import SlideText from './components/SlideText'
+
 
 class App extends Component {
+  state = {
+    sip : [],
+}
+
+  constructor() {
+    super()
+    fetch('http://localhost:5000/mock')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({sip : JSON.parse(res)})
+        console.log(this.state.sip[1].text)
+      })
+
+  }
+
   render () {
     return (
       <div className="App">
@@ -13,7 +30,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {this.state.sip.map(slide => <SlideText slide={slide} /> )}
       </div>
+
     )
   }
 }
---
+++ src/components/SlideText.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SlideText = ({slide}) => {
+  return (
+    <div>
+      <p>{slide.text}</p>
+    </div>
+  )
+}
+
+export default SlideText
