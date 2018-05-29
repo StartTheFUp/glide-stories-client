9a61f80eb57c3be5807799b92620ae902afe2448
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-05-29 18:42:49 +0200

    show slide 1 by 1

--- src/App.js
+++ src/App.js
@@ -6,21 +6,30 @@ import SlideText from './components/SlideText'
 
 class App extends Component {
   state = {
+    currentStep : 1,
     sip : [],
-}
+  }
+
+  handleNavigation = () => {
+    this.setState({currentStep : this.state.currentStep + 1})
+    console.log('lol')
+  }
 
   constructor() {
     super()
     fetch('http://localhost:5000/mock')
       .then(res => res.json())
-      .then(res => {
-        this.setState({sip : JSON.parse(res)})
-        console.log(this.state.sip[1].text)
-      })
-
+      .then(res => this.setState({sip : JSON.parse(res)}))
   }
 
   render () {
+    if (this.state.currentStep < this.state.sip.length) {
+      return (
+        <div>
+          <SlideText handleNavigation={this.handleNavigation} slide={this.state.sip[this.state.currentStep]} />
+        </div>
+      )
+    }
     return (
       <div className="App">
         <header className="App-header">
@@ -30,9 +39,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        {this.state.sip.map(slide => <SlideText slide={slide} /> )}
       </div>
-
     )
   }
 }
--- src/components/SlideText.js
+++ src/components/SlideText.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const SlideText = ({slide}) => {
+const SlideText = ({handleNavigation, slide}) => {
   return (
-    <div>
+    <div onClick={() => handleNavigation()}>
       <p>{slide.text}</p>
     </div>
   )
