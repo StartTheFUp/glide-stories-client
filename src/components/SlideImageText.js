f8c2aaffeeaba72abee35a15da327563bdfc2dd6
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-06-01 16:47:15 +0200

    create image slide js and css

---
+++ src/components/SlideImageText.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './SlideImageText.css'
+
+const SlideImageText = ({ text, image }) =>
+  <div className='SlideImage'>
+    <p>{text}</p>
+  </div>
+
+export default SlideImageText
