628a213ad01a7d9732c7615d90c1b0a23434b948
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-06-04 18:29:23 +0200

    change background image

--- src/components/SlideIntro.js
+++ src/components/SlideIntro.js
@@ -2,9 +2,10 @@ import React from 'react'
 import './SlideIntro.css'
 
 const SlideIntro = ({ subTitle, title, image }) =>
-  <div className='SlideIntro'>
+<div style={{backgroundImage: `url(${image})`}} className='SlideIntro'>
     <p>{title}</p>
     <p>{subTitle}</p>
+
   </div>
 
 export default SlideIntro
