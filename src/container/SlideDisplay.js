f87f6977edf94b7eba3349f004eff1b667b23b17
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-06-04 09:53:37 +0200

    update SlideDisplay

--- src/container/SlideDisplay.js
+++ src/container/SlideDisplay.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import SlideText from '../components/SlideText'
+import SlideIntro from '../components/SlideIntro'
+import SlideImageText from '../components/SlideImageText'
 import './SlideDisplay.css'
 
+
 const slideComponents = {
-  text: SlideText
+  text: SlideText,
+  intro: SlideIntro,
+  imageText: SlideImageText
+
 }
 
 const SlideDisplay = ({handleNext, handlePrevious, slide}) => {
