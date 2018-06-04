57b8354166de02062a3d8319bb8ed46716f23242
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-06-04 18:33:30 +0200

    add button with link

---
+++ src/components/SlideCallToAction.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './SlideIntro.css'
+import { Button } from 'semantic-ui-react'
+
+
+const SlideCallToAction = ({ subTitle, title, image, btnLink, btnText  }) =>
+  <div className='SlideIntro'>
+    <p>{title}</p>
+    <p>{subTitle}</p>
+    <Button><a href={btnLink}>{btnText}</a></Button>
+  </div>
+
+export default SlideCallToAction
