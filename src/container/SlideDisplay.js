70eaa370b4549ec1f963bfca6165f28d689746ce
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-06-04 18:30:14 +0200

    add other types of slides

--- src/container/SlideDisplay.js
+++ src/container/SlideDisplay.js
@@ -2,14 +2,15 @@ import React from 'react'
 import SlideText from '../components/SlideText'
 import SlideIntro from '../components/SlideIntro'
 import SlideImageText from '../components/SlideImageText'
+import SlideCallToAction from '../components/SlideCallToAction'
 import './SlideDisplay.css'
 
 
 const slideComponents = {
   text: SlideText,
   intro: SlideIntro,
-  imageText: SlideImageText
-
+  imageText: SlideImageText,
+  callToAction: SlideCallToAction,
 }
 
 const SlideDisplay = ({handleNext, handlePrevious, slide}) => {
