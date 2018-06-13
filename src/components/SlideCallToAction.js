843dee15420c439b015fe175f1343db4ef99f941
Capucine Leclerc <capucine_49@hotmail.com>
2018-06-13 18:42:02 +0200

    correction subtitle / subTitle

--- src/components/SlideCallToAction.js
+++ src/components/SlideCallToAction.js
@@ -3,11 +3,11 @@ import '../container/SlideDisplay.css'
 import './SlideIntro.css'
 import { Button } from 'semantic-ui-react'
 
-const SlideCallToAction = ({ subTitle, title, image, btnLink, btnText }) =>
+const SlideCallToAction = ({ subtitle, title, image, btnLink, btnText }) =>
   <div className='SlideIntro' style={{backgroundImage: `url(${image})`}}>
     <div className="SlideIntroText">
       <h1>{title}</h1>
-      <h2>{subTitle}</h2>
+      <h2>{subtitle}</h2>
       <Button><a href={btnLink}>{btnText}</a></Button>
     </div>
   </div>
--- src/components/SlideIntro.js
+++ src/components/SlideIntro.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './SlideIntro.css'
 
-const SlideIntro = ({ subTitle, title, image }) =>
+const SlideIntro = ({ subtitle, title, image }) =>
   <div className='SlideIntro' style={{backgroundImage: `url(${image})`}}>
     <div>
       <h1>{title}</h1>
-      <h2>{subTitle}</h2>
+      <h2>{subtitle}</h2>
     </div>
   </div>
 
