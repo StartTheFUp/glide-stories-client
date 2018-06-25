d861102f2173f45f224d91f8c3efd652f40730a6
Capucine Leclerc <capucine_49@hotmail.com>
2018-06-25 13:36:06 +0200

    remove initial fetch

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import { fetchInitialState } from './store.js'
-
-fetchInitialState()
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
