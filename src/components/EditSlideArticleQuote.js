234814420283b7d0906bf224551186642b6c2428
Pierre Langevin <pierrelangev1@gmail.com>
2018-06-25 23:55:12 +0200

    edit in progess

---
+++ src/components/EditSlideArticleQuote.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './EditSlideArticleQuote.css'
+import '../container/SlideDisplay.css'
+import '../container/SlideEditor.js'
+
+const EditSlideArticleQuote = ({ slide, onChange }) => {
+  const { source, authorName, text } = slide || {}
+  return (
+    <div className="__SlideDisplay">
+      <div className="EditText">
+        <input
+          maxLength="300"
+          value={text}
+          onChange={event => onChange(event, 'text')} />
+        <input
+          maxLength="300"
+          value={authorName}
+          onChange={event => onChange(event, 'authorName')} />
+        <input
+          maxLength="300"
+          value={source}
+          onChange={event => onChange(event, 'source')} />
+      </div>
+    </div>
+  )
+}
+export default EditSlideArticleQuote
--- src/container/SlideEditor.js
+++ src/container/SlideEditor.js
@@ -1,6 +1,19 @@
 import React, { Fragment, Component } from 'react'
 import EditSlideText from '../components/EditSlideText'
+import EditSlideArticleQuote from '../components/EditSlideArticleQuote'
+// import EditSlideArticleQuote from '../components/EditSlideArticleQuote'
+// import EditSlideArticleQuote from '../components/EditSlideArticleQuote'
+// import EditSlideArticleQuote from '../components/EditSlideArticleQuote'
 import { actions } from '../store.js'
+
+const EditSlideComponents = {
+  text: EditSlideText,
+  // intro: SlideIntro,
+  // image: SlideImage,
+  // callToAction: SlideCallToAction,
+  // tweet: SlideTweet,
+  article: EditSlideArticleQuote
+}
 
 class SlideEditor extends Component {
   componentDidMount() {
@@ -9,14 +22,25 @@ class SlideEditor extends Component {
       .then(actions.loadSip)
       .then(() => actions.handleNextSip())
   }
-  // dans le store utiliser le
   render() {
-    console.log(this.props, this.props.sip.slides[this.props.currentStep])
+    console.log('heyhey', this.props, this.props.sip.slides[this.props.currentStep])
+    const { sip, currentStep } = this.props
+    const slide = sip.slides[currentStep]
+    if (!slide) return 'loading'
     return (
       <Fragment>
-        <EditSlideText
+        {EditSlideComponents[slide.type] && EditSlideComponents[slide.type]({
+          slide,
+          onChange: (event, key) => actions.updateSlide({ [key]: event.target.value })
+        })}
+        <button onClick={actions.handlePreviousSip}>Previous</button>
+        <button onClick={actions.handleNextSip}>Next</button>
+        {/* <EditSlideText
           slide={this.props.sip.slides[this.props.currentStep]}
           onChange={(event, key) => actions.updateSlide({ [key]: event.target.value })} />
+        <EditSlideArticleQuote
+          slide={this.props.sip.slides[this.props.currentStep]}
+          onChange={(event, key) => actions.updateSlide({ [key]: event.target.value })} /> */}
       </Fragment>
     )
   }
