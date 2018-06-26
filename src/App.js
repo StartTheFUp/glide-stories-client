af6b3795e627471a1763acb43f00c85f3dd1056f
Capucine Leclerc <capucine_49@hotmail.com>
2018-06-26 18:26:12 +0200

    add page Stories.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 import SlideDisplay from './container/SlideDisplay.js'
 import { store } from './store.js'
 import SlideEditor from './container/SlideEditor'
+import Stories from './container/Stories.js'
 import { Router } from '@reach/router'
 
 class App extends Component {
@@ -18,6 +19,7 @@ class App extends Component {
   render() {
     return (
       <Router>
+        <Stories path='/stories' />
         <SlideDisplay {...this.state} path='/:id' />
         <SlideEditor {...this.state} path='/edit/:id' />
       </Router>
