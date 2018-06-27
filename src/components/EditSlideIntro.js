dbb7651d191a4e18807d6ce488c3ff8994df7840
Louise Foussat <louisefoussat@gmail.com>
2018-06-27 03:34:25 +0200

    ajust slide intro -> places elements into the slide, add style

--- src/components/EditSlideIntro.js
+++ src/components/EditSlideIntro.js
@@ -1,22 +1,29 @@
 import React from 'react'
 import './EditSlideIntro.css'
 import '../container/SlideDisplay.css'
+import './SlideIntro.css'
 import '../container/SlideEditor.js'
 
 const EditSlideIntro = ({ slide, onChange }) => {
   const { imageUrl, title, subtitle } = slide || {}
   return (
     <div className='__SlideDisplay'>
-      <div className='EditIntro' style={{backgroundImage: `url("${imageUrl}")`}}>
-        <input type='file' />
-        <h1><input
-          maxLength='60'
-          value={title}
-          onChange={event => onChange(event, 'title')} /></h1>
-        <h2><input
-          maxLength='250'
-          value={subtitle}
-          onChange={event => onChange(event, 'subtitle')} /></h2>
+      <div className='SlideIntro' style={{backgroundImage: `url("${imageUrl}")`}}>
+        <div>
+          <input style={{flexGrow: 0.5}} type='file' />
+          <h1><textarea
+            maxLength='60'
+            rows='2'
+            wrap='hard'
+            value={title}
+            onChange={event => onChange(event, 'title')} /></h1>
+          <h2 style={{flex: 0.7}}><textarea
+            maxLength='250'
+            rows='2'
+            wrap='hard'
+            value={subtitle}
+            onChange={event => onChange(event, 'subtitle')} /></h2>
+        </div>
       </div>
     </div>
   )
