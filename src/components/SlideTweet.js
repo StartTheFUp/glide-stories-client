74eb6a484e07e5ad4a6a67da97bcad23e477e9a2
Louise Foussat <louisefoussat@gmail.com>
2018-06-27 03:41:47 +0200

    update : publication_date -> publicationDate

--- src/components/SlideTweet.js
+++ src/components/SlideTweet.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './SlideScrapping.css'
 
-const SlideTweet = ({ imageUrl, authorName, authorScreenName, text, publication_date }) =>
+const SlideTweet = ({ imageUrl, authorName, authorScreenName, text, publicationDate }) =>
   <div className='quote'>
     <span className='quoteHead'>
       <img src={imageUrl} alt='profile_picture' />
@@ -9,7 +9,7 @@ const SlideTweet = ({ imageUrl, authorName, authorScreenName, text, publication_
       <p className='greyInfo'>{authorScreenName}</p>
     </span>
     <p>{text}</p>
-    <p className='greyInfo'>{publication_date}</p>
+    <p className='greyInfo'>{publicationDate}</p>
   </div>
 
 export default SlideTweet
