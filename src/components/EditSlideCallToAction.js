7cb395d8bb055452297f1b9c02c6fe46587303f5
Louise Foussat <louisefoussat@gmail.com>
2018-06-27 03:35:48 +0200

    ajust edit call to action component => add the rigtht props, places elements intro the slide, add style

--- src/components/EditSlideCallToAction.js
+++ src/components/EditSlideCallToAction.js
@@ -2,22 +2,38 @@ import React from 'react'
 import './EditSlideCallToAction.css'
 import '../container/SlideDisplay.css'
 import '../container/SlideEditor.js'
+import './SlideIntro.css'
 
 const EditSlideCallToAction = ({ slide, onChange }) => {
-  const { imageUrl, title, subtitle } = slide || {}
+  const { imageUrl, title, subtitle, btnLink, btnText } = slide || {}
   return (
     <div className='__SlideDisplay'>
-      <div className='EditText'>
-        <div className='SlideIntro' style={{backgroundImage: `url("${imageUrl}")`}}>
-          <input type='file' />
-          <input
-            maxLength='300'
+      <div className='SlideIntro' style={{backgroundImage: `url("${imageUrl}")`}}>
+        <div>
+          <input style={{flexGrow: 0.5}} type='file' />
+          <h1><textarea
+            maxLength='60'
+            rows='2'
+            wrap='hard'
             value={title}
-            onChange={event => onChange(event, 'title')} />
-          <input
-            maxLength='300'
+            onChange={event => onChange(event, 'title')} /></h1>
+          <h2 style={{flex: 0.7}}><textarea
+            maxLength='250'
+            rows='2'
+            wrap='hard'
             value={subtitle}
-            onChange={event => onChange(event, 'subtitle')} />
+            onChange={event => onChange(event, 'subtitle')} /></h2>
+          <textarea
+            className='editButton'
+            maxLength='30'
+            rows='1'
+            wrap='hard'
+            value={btnText}
+            onChange={event => onChange(event, 'btnText')} />
+          <input
+            maxLength='500'
+            value={btnLink}
+            onChange={event => onChange(event, 'btnLink')} />
         </div>
       </div>
     </div>
