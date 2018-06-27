8adbc5225a224d6cd33e9bea81d8afe0c8dd33d6
Louise Foussat <louisefoussat@gmail.com>
2018-06-27 03:35:07 +0200

    ajust edit image component => places elements intro the slide, add style

--- src/components/EditSlideImage.js
+++ src/components/EditSlideImage.js
@@ -2,21 +2,28 @@ import React from 'react'
 import './EditSlideImage.css'
 import '../container/SlideDisplay.css'
 import '../container/SlideEditor.js'
+import './SlideImage.css'
 
 const EditSlideImage = ({ slide, onChange }) => {
   const { imageUrl, text } = slide || {}
   return (
     <div className='__SlideDisplay'>
       <div className='EditImage'>
-        <div className='SlideImage'>
-          <div className='img' style={{backgroundImage: `url("${imageUrl}")`}} />
-          <input type='file' />
-          <form>
-          <input
-            maxLength='120'
-            value={slide && text}
-            onChange={event => onChange(event, 'text')} />
-          </form>
+        <div className='SlideImage' style={{display: 'flex'}}>
+          <input type='file' style={{ padding: '0 10px' }}/>
+          <div className='img' style={{
+            backgroundImage: `url("${imageUrl}")`,
+            margin: '10px 0'
+          }} />
+          <div className='EditText'>
+            <textarea
+              maxLength='120'
+              rows='2'
+              wrap='hard'
+              placeholer='Type your text here'
+              value={slide && text}
+              onChange={event => onChange(event, 'text')} />
+          </div>
         </div>
       </div>
     </div>
