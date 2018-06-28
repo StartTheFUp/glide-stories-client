f100c8a827ec06236608948414969055a25bb785
Louise Foussat <louisefoussat@gmail.com>
2018-06-28 17:38:02 +0200

    comment fetch initial state (from mocks)

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import { fetchInitialState } from './store.js'
+// import { fetchInitialState } from './store.js'
 
-fetchInitialState()
+// fetchInitialState()
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
