a0c728dbc0264bacfc2ca248522a8614bf195828
Capucine Leclerc <capucine_49@hotmail.com>
2018-06-29 10:12:19 +0200

    style mysips page

--- src/components/Previewsip.js
+++ src/components/Previewsip.js
@@ -3,19 +3,29 @@ import '../container/Stories.js'
 import './Previewsip.css'
 import { Button, Segment } from 'semantic-ui-react'
 
-
 const Previewsip = ({ title, slideIntro, embed, publicUrl, editSip, deleteSip }) =>
-    <Segment padded>
+  <Segment padded>
     <h3>{title}</h3>
+    <div className="SipShare">
+
+      <div className="BoxShare">
+        <h4>Embed</h4>
+        <textarea>{embed}</textarea>
+      </div>
+
+      <div className="BoxShare">
+        <h4>Public URL</h4>
+        <div className="url">{publicUrl}</div>
+      </div>
+    </div>
+
     <div className="PreviewBtn">
       <div className="Previewsip">{slideIntro}</div>
       <div className="Btn">
-        <Button basic color='blue' onClick={() => embed}>Embed</Button>
-        <Button basic color='blue' onClick={() => publicUrl}>Public URL</Button>
         <Button basic color='blue' onClick={() => editSip}>Edit</Button>
         <Button basic color='red' onClick={() => deleteSip}>Delete</Button>
       </div>
     </div>
-    </Segment>
+  </Segment>
 
 export default Previewsip
--- src/container/Stories.js
+++ src/container/Stories.js
@@ -3,29 +3,32 @@ import './Stories.css'
 import Previewsip from '../components/Previewsip.js'
 import { actions } from '../store.js'
 
-
 class Stories extends Component {
   componentDidMount() {
-    fetch(`http://localhost:5000/sips`)
-      .then(res => res.json())
-      .then(actions.displaySips)
+    fetch(`http://localhost:5001/sips`)
+      .then(sips => sips.json())
+      .then(actions.loadSips)
   }
+
   render() {
+    console.log(this.props.sip.slides[0])
+
     const mysips = this.props.sips.map(mysip =>
       <Previewsip
         key={mysip.id}
         title={mysip.title}
-      />
-    )
-
+        slideIntro='rien'
+        embed='je suis un morceau de code'
+        publicUrl='http://websips.com/view/453789' />)
     return (
       <div className='Stories'>
         <h1>My sips</h1>
-        {mysips}
+        <div className="SipContainer">
+          {mysips}
+        </div>
       </div>
     )
   }
 }
 
 export default Stories
-
