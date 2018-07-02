4ade6af9d81f7a580a1ffbba60d2e661a9973cf3
Alex Bui <alxbui14@gmail.com>
2018-07-02 23:51:43 +0200

    edition slideArticle ON

--- src/components/EditSlideArticleQuote.js
+++ src/components/EditSlideArticleQuote.js
@@ -4,19 +4,30 @@ import '../container/SlideDisplay.css'
 import '../container/SipEditor.js'
 
 const EditSlideArticleQuote = ({ slide, onChange }) => {
-  const { articleUrl, sourceImage, sourceName, authorName, text, publicationDate } = slide || {}
+  const { sourceImage, sourceName, authorName, text, publicationDate } = slide || {}
+  console.log(slide)
   return (
     <div className='__SlideDisplay'>
       <div className='EditArticle' style={{flexDirection: 'column'}}>
-        <input
-          maxLength='500'
-          value={articleUrl}
-          onChange={event => onChange(event, 'articleUrl')} />
         <div className='quote' style= {{justifyContent: 'center'}}>
           <span className='quoteHead'>
             <img src={sourceImage} alt='Article source icon' />
-            <p className='authorName'>{sourceName}</p>
-            <p className='greyInfo'>{authorName}</p>
+            <textarea
+              className='authorName'
+              maxLength='120'
+              rows='2'
+              wrap='hard'
+              placeholer={sourceName}
+              value={sourceName}
+              onChange={event => onChange(event, 'sourceName')} />
+            <textarea
+              className='greyInfo'
+              maxLength='120'
+              rows='2'
+              wrap='hard'
+              placeholer={authorName}
+              value={authorName}
+              onChange={event => onChange(event, 'authorName')} />
           </span>
           <div className='EditText'>
             <textarea
