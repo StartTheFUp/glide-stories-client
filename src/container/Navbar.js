ddaac4b2c65fb7fb441f7a7e972c0f75cea4e0cc
Pierre Langevin <pierrelangev1@gmail.com>
2018-07-04 14:24:17 +0200

    LoginForm SignUpForm redirection and errors

--- src/container/Navbar.js
+++ src/container/Navbar.js
@@ -7,14 +7,14 @@ class Navbar extends Component {
   render() {
     return (
       <div className='Navbar'>
-        <div class="ui menu">
-          <div class="item">
+        <div className="ui menu">
+          <div className="item">
             <h3>Websips</h3>
           </div>
-          <a class="item" href="/mysips">My Sips</a>
-          <div class="right menu">
-            <a class="item"><Icon name='user' /> My account</a>
-            <a class="item"><Icon name='user times' /> Log out</a>
+          <a className="item" href="/mysips">My Sips</a>
+          <div className="right menu">
+            <a className="item"><Icon name='user' /> My account</a>
+            <a className="item" onClick={this.props.logout}><Icon name='user times' /> Log out</a>
           </div>
         </div>
 
