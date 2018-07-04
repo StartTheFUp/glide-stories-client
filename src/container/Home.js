b6b2d167b0bf1fb312c3a92ab32fd21fe7c8a964
Pierre Langevin <pierrelangev1@gmail.com>
2018-07-04 15:35:19 +0200

    navbar REACT

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import LoginForm from '../components/LoginForm'
 import { Redirect, navigate } from '@reach/router'
 import SignUpForm from '../components/SignUpForm'
--- src/container/Navbar.js
+++ src/container/Navbar.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react'
 import './Navbar.css'
-import { Icon } from 'semantic-ui-react'
+import { Icon, Menu } from 'semantic-ui-react'
 
 class Navbar extends Component {
-
   render() {
     return (
-      <div className='Navbar'>
-        <div className="ui menu">
-          <div className="item">
-            <h3>Websips</h3>
-          </div>
-          <a className="item" href="/mysips">My Sips</a>
-          <div className="right menu">
-            <a className="item"><Icon name='user' /> My account</a>
-            <a className="item" onClick={this.props.logout}><Icon name='user times' /> Log out</a>
-          </div>
-        </div>
+      // <div className='Navbar'>
+      //   <div className="ui menu">
+      //     <div className="item">
+      //       <h3>Websips</h3>
+      //     </div>
+      //     <a className="item" href="/mysips">My Sips</a>
+      //     <div className="right menu">
+      //       <a className="item"><Icon name='user' /> My account</a>
+      //       <a className="item" onClick={this.props.logout}><Icon name='user times' /> Log out</a>
+      //     </div>
+      //   </div>
+      // </div>
 
-      </div>
+      <Menu>
+
+        <Menu.Item name='editorials'>
+          <h3>Websips</h3>
+        </Menu.Item>
+
+        <Menu.Item name='reviews'>
+          <a className="item" href="/mysips">My Sips</a>
+        </Menu.Item>
+
+        <Menu.Item name='upcomingEvents'>
+          <a className="item"><Icon name='user' /> My account</a>
+        </Menu.Item>
+
+        <Menu.Item name='upcomingEvents'>
+          <a className="item" onClick={this.props.logout}><Icon name='user times' /> Log out</a>
+        </Menu.Item>
+
+      </Menu>
     )
   }
 }
