bdfbd6a1d863237b6213f0487f0d849c584c658b
Louise Foussat <louisefoussat@gmail.com>
2018-07-05 05:24:32 +0200

    coup de polish

--- src/components/Newsip.js
+++ src/components/Newsip.js
@@ -7,17 +7,14 @@ import { Link } from '@reach/router'
 
 const Newsip = () =>
   <Grid.Column>
-    <Link to={`/mysips/new`} >
-
+    <Link to='/mysips/new'>
       <Segment>
         <div className="BtnNewsip">
           <img src={Addsip} width="100" alt="add sip icon" />
           <h3>Add a new sip</h3>
         </div>
-
       </Segment>
     </Link>
-
   </Grid.Column>
 
 export default Newsip
