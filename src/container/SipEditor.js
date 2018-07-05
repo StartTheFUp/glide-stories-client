76cbf58320eef09db7c8550c699ab670c0e20bbf
Alex Bui <alxbui14@gmail.com>
2018-07-05 04:04:02 +0200

    relou

--- src/container/SipEditor.js
+++ src/container/SipEditor.js
@@ -145,7 +145,7 @@ class SipEditor extends Component {
               })}
             </div>
             <ModalInputUrl
-              open= {this.props.insertUrl}git 
+              open= {this.props.insertUrl}
               onClose={() => navigate(`/edit/${sip.id}`)}
               onChange={e => actions.updateUrl(e.target.value)}
               onSubmit={() => actions.addSlide(this.props.type)}
