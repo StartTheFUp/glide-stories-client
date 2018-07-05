d1c39f7968c6c87dcfbd53c96159a6091f77cd1e
Alex Bui <alxbui14@gmail.com>
2018-07-05 06:53:16 +0200

    création article et tweet via dropdown + modal fonctionnelle, mais route modale  améliorer

--- src/components/AddSlideBtn.js
+++ src/components/AddSlideBtn.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { navigate } from '@reach/router'
+import { actions } from '../store.js'
+import 'semantic-ui-css/semantic.min.css'
 import { Dropdown } from 'semantic-ui-react'
 import './AddSlideBtn.css'
-import 'semantic-ui-css/semantic.min.css'
 
-// const tweet = () => actions.tweetType()
-// const article = () => actions.articleType()
+const tweet = () => actions.tweetType()
+const article = () => actions.articleType()
 
 const AddSlideBtn = props =>
   <div className='add-slide'>
@@ -15,8 +17,8 @@ const AddSlideBtn = props =>
         <Dropdown.Item onClick={() => props.addSlide('text')}>TEXT</Dropdown.Item>
         <Dropdown.Item onClick={() => props.addSlide('image')}>IMAGE + TEXT</Dropdown.Item>
         <Dropdown.Item onClick={() => props.addSlide('callToAction')}>CALL TO ACTION</Dropdown.Item>
-        <Dropdown.Item onClick={() => props.showModal('tweet') }>TWEET</Dropdown.Item>
-        <Dropdown.Item onClick={() => props.showModal('article')}>ARTICLE</Dropdown.Item>
+        <Dropdown.Item onClick={() => { navigate(`/edit/${props.id}/url`); tweet() }}>TWEET</Dropdown.Item>
+        <Dropdown.Item onClick={() => { navigate(`/edit/${props.id}/url`); article() }}>ARTICLE</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   </div>
